343b36215b4077c788dd8be7f942b07dcfd72cf3
Kate <restacomesei@gmail.com>
2020-12-18 17:28:16 +0100

    next project

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -25,3 +25,29 @@ var divide = function divide() {
 
 btnMul.addEventListener('click', multiply);
 btnSub.addEventListener('click', divide);
+var btnPlus = document.querySelector('.card__btn--plus');
+var btnMinus = document.querySelector('.card__btn--minus');
+var btnColor = document.querySelector('.card__btn--color');
+var text = document.querySelector('.card__text--lorem');
+var i = 18;
+
+var incrase = function incrase() {
+  i = i + 5;
+  text.style.fontSize = i + 'px';
+};
+
+var decrease = function decrease() {
+  i = i - 5;
+  text.style.fontSize = i + 'px';
+};
+
+var colorChanger = function colorChanger() {
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  text.style.color = "rgb(" + r + "," + g + "," + b + ")";
+};
+
+btnPlus.addEventListener('click', incrase);
+btnMinus.addEventListener('click', decrease);
+btnColor.addEventListener('click', colorChanger);
--- js/main.js
+++ js/main.js
@@ -24,3 +24,32 @@ const divide = () => {
 
 btnMul.addEventListener('click', multiply);
 btnSub.addEventListener('click', divide);
+
+const btnPlus = document.querySelector('.card__btn--plus');
+const btnMinus = document.querySelector('.card__btn--minus');
+const btnColor = document.querySelector('.card__btn--color');
+const text = document.querySelector('.card__text--lorem');
+
+let i = 18;
+
+const incrase = () =>  {
+    i = i + 5;
+    text.style.fontSize = i + 'px';
+};
+
+const decrease = () =>  {
+    i = i - 5;
+    text.style.fontSize = i + 'px';
+};
+
+const colorChanger = () =>  {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    text.style.color = "rgb(" + r + "," + g + "," + b + ")";
+};
+
+btnPlus.addEventListener('click', incrase);
+btnMinus.addEventListener('click', decrease);
+btnColor.addEventListener('click', colorChanger);
+
